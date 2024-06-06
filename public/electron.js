77a433a6ca898206a19a0196460b97174d33264e
RobertGemmaJr <rgemma.jr@gmail.com>
2024-06-06 14:17:02 -0400

    feat: Add default task config file to fall back on

--- public/electron.js
+++ public/electron.js
@@ -231,10 +231,14 @@ ipc.on("syncCredentials", (event) => {
 
 // Get task configuration from environment
 ipc.handle("syncConfig", (event, studyId, participantId) => {
-  const configPath = getConfigPath(studyId, participantId);
-
-  // TODO: Needs to return default config if it can't find a json file
-  const config = JSON.parse(fs.readFileSync(path.resolve(configPath, participantId + ".json")));
+  let config;
+  try {
+    const configPath = getConfigPath(studyId, participantId);
+    config = JSON.parse(fs.readFileSync(path.resolve(configPath, participantId + ".json")));
+  } catch (error) {
+    log.info("Custom taskConfig file was not found:", studyId, participantId);
+    config = JSON.parse(fs.readFileSync(path.resolve(__dirname, "config/defaultTaskConfig.json")));
+  }
   return config;
 });
 
@@ -247,7 +251,6 @@ ipc.on("data", (event, args) => {
     studyID = args.study_id;
     preSavePath = path.resolve(dir, `pid_${participantID}_${today.getTime()}.json`);
     startTrial = args.trial_index;
-    log.warn(preSavePath);
     stream = fs.createWriteStream(preSavePath, { flags: "ax+" });
     stream.write("[");
     fileCreated = true;
